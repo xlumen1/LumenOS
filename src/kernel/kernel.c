6928fe668a464b35ba4d6135a51bd1b895c894d0
Ncreate101 <gstoermer1@gmail.com>
2025-01-22 22:48:03 -0500

    Update Makefile And Update Kernel (BUGGED)

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,5 +1,10 @@
 #include "libk/vga/vga.h"
+
+#define DEFCOL VGA_COLOR(VGA_WHITE, VGA_BLACK)
+
 void kmain() {
     vga_clear();
-    vga_set('A', VGA_COLOR(VGA_WHITE, VGA_BLACK), 0, 0);
+    for (char i = 0; i < 256; i++) {
+        vga_put((char)i, DEFCOL);
+    }
 }
