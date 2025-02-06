b86951e5b78a9baaea78f09e39cdfb14f7731ff5
Ncreate101 <>
2025-02-06 23:45:07 +0000

    Implement itoa

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,8 +1,12 @@
 #include "libk/vga/vga.h"
+#include "libk/util/util.h"
 
 #define DEFCOL VGA_COLOR(VGA_WHITE, VGA_BLACK)
 
 void kmain() {
     vga_clear();
     vga_write("Lumen OS", DEFCOL, 8);
+    vga_col = 0;
+    vga_row = 1;
+    vga_write(itoa(42, 10), DEFCOL, 2);
 }
