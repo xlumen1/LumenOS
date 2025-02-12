4ca4c90649509b91725971cfe0fb136ab7013d81
Ncreate101 <gstoermer1@gmail.com>
2025-02-11 20:27:49 -0500

    Moved time to header, made header hardcoded

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,7 +5,7 @@
 #include "libk/time/time.h"
 
 #define DEFCOL VGA_COLOR(VGA_WHITE, VGA_BLUE)
-#define HEADERCOL VGA_COLOR(VGA_BLUE, VGA_WHITE)
+#define HEADERCOL VGA_COLOR(VGA_BLUE, VGA_LIGHT_GREY)
 
 #define KERNEL_VERSION "v0.0.3-prototype"
 
@@ -13,14 +13,15 @@ void kmain()
 {
     vga_init();
     vga_clear(DEFCOL);
-    char *txt = "Lumen Kernel";
+    /*char *txt = "Lumen Kernel";
     for (unsigned char i = 0; i < (VGA_WIDTH / 2) + 5 - strlen(txt); i++)
         vga_put('=', HEADERCOL);
     vga_put('[', HEADERCOL);
     vga_print(txt, HEADERCOL);
     vga_put(']', HEADERCOL);
     for (unsigned char i = 0; i < (VGA_WIDTH / 2) + 5 - strlen(txt); i++)
-        vga_put('=', HEADERCOL);
+        vga_put('=', HEADERCOL);*/
+    vga_print("=================================[Lumen Kernel]=================================", HEADERCOL);
     vga_print(KERNEL_VERSION, DEFCOL);
     vga_pos(0, 4);
     gdt_encode((unsigned char *)0x20, (struct GDT){0, 0, 0, 0});
@@ -35,41 +36,41 @@ void kmain()
     while (1)
     {
         struct time t = get_time();
-        vga_pos(0, 2);
+        vga_pos(4, 0);
         if (t.month < 10)
-            vga_put('0', DEFCOL);
-        vga_print(itoa(t.month, 10), DEFCOL);
-        vga_put('/', DEFCOL);
+            vga_put('0', HEADERCOL);
+        vga_print(itoa(t.month, 10), HEADERCOL);
+        vga_put('/', HEADERCOL);
         if (t.day < 10)
-            vga_put('0', DEFCOL);
-        vga_print(itoa(t.day, 10), DEFCOL);
-        vga_put('/', DEFCOL);
+            vga_put('0', HEADERCOL);
+        vga_print(itoa(t.day, 10), HEADERCOL);
+        vga_put('/', HEADERCOL);
         if (t.century < 10)
-            vga_put('0', DEFCOL);
-        vga_print(itoa(t.century, 10), DEFCOL);
+            vga_put('0', HEADERCOL);
+        vga_print(itoa(t.century, 10), HEADERCOL);
         if (t.year < 10)
-            vga_put('0', DEFCOL);
-        vga_print(itoa(t.year, 10), DEFCOL);
-        vga_newline();
+            vga_put('0', HEADERCOL);
+        vga_print(itoa(t.year, 10), HEADERCOL);
+        vga_pos(21, 0);
         if (t.hour - 5 < 10) {
-            vga_put('0', DEFCOL);
+            vga_put('0', HEADERCOL);
             if (t.hour == 0)
-                vga_put('0', DEFCOL);
+                vga_put('0', HEADERCOL);
         }
-        vga_print(itoa(t.hour - 5, 10), DEFCOL);
-        vga_put(':', DEFCOL);
+        vga_print(itoa(t.hour - 5, 10), HEADERCOL);
+        vga_put(':', VGA_COLOR(VGA_BLUE, VGA_WHITE));
         if (t.minute < 10) {
-            vga_put('0', DEFCOL);
+            vga_put('0', HEADERCOL);
             if (t.minute == 0)
-                vga_put('0', DEFCOL);
+                vga_put('0', HEADERCOL);
         }
-        vga_print(itoa(t.minute, 10), DEFCOL);
-        vga_put(':', DEFCOL);
+        vga_print(itoa(t.minute, 10), HEADERCOL);
+        vga_put(':', VGA_COLOR(VGA_BLUE, VGA_WHITE));
         if (t.second < 10) {
-            vga_put('0', DEFCOL);
+            vga_put('0', HEADERCOL);
             if (t.second == 0)
-                vga_put('0', DEFCOL);
+                vga_put('0', HEADERCOL);
         }
-        vga_print(itoa(t.second, 10), DEFCOL);
+        vga_print(itoa(t.second, 10), HEADERCOL);
     }
 }
