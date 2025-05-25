4bb1421c7f40d27d801b093996c6745efd849304
Ncreate101 <gstoermer1@gmail.com>
2025-05-24 22:58:35 -0400

    Fix error caused by fixing warnings

--- src/kernel/libk/vga/text/print.c
+++ src/kernel/libk/vga/text/print.c
@@ -1,6 +1,6 @@
 #include "../vga.h"
 
 // Write a string to the VGA buffer
-void vga_print(char* a, unsigned char color) {
+void vga_print(const char* a, unsigned char color) {
     vga_write(a, color, strlen(a));
 }
