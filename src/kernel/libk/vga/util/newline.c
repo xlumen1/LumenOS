2a07eb281398c639230e86decc481d8bebe9c113
Ncreate101 <>
2025-05-28 17:08:41 +0000

    Add scrolling

--- src/kernel/libk/vga/util/newline.c
+++ src/kernel/libk/vga/util/newline.c
@@ -3,6 +3,8 @@
 // Move the cursor to the next line
 void vga_newline() {
     vga_pos(0, vga_row + 1);
-    if (vga_row > VGA_HEIGHT)
+    if (vga_row > VGA_HEIGHT - 1) {
         vga_scroll(1);
+	vga_pos(0, vga_row - 1);
+    }
 }
--- src/kernel/libk/vga/util/scroll.c
+++ src/kernel/libk/vga/util/scroll.c
@@ -18,9 +18,9 @@ void vga_scroll(uint8_t lines) {
         chars_to_copy * sizeof(uint16_t)
     );
 
-    // for (int row = VGA_HEIGHT - lines; row < VGA_HEIGHT; row++) {
-    //     for (int col = 0; col < VGA_WIDTH; col++) {
-    //         VGA_ADDRESS[row * VGA_WIDTH + col] = ' ' | (0x07 << 8);
-    //     }
-    // }
+    for (int row = VGA_HEIGHT - lines; row < VGA_HEIGHT; row++) {
+         for (int col = 0; col < VGA_WIDTH; col++) {
+             VGA_ADDRESS[row * VGA_WIDTH + col] = '\0' | (0x07 << 8);
+    	}
+    }
 }
